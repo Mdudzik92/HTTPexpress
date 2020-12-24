fd85c7ac589e1af61a60138ea506d34df27dd9d3
Matt Dudzik <mdudzik92@gmail.com>
2020-12-24 10:08:06 -0600

    Hello from Express

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
+// Bringing in Express
 const express = require("express");
 const path = require("path");
 
+// Initializing Express
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-	res.send("Hello From Express");
+	res.send("<h1>Hello From Express</h1>");
 });
 
+// Setting Express to listen on port 5000
 app.listen(5000, () => console.log(`Server started on 5000`));
