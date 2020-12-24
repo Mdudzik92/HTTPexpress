249136f44d1217f800518c0732edd3a8bfb687ad
Matt Dudzik <mdudzik92@gmail.com>
2020-12-24 10:10:39 -0600

    Hello json in body

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-	res.send({ msg: "Hello" });
+	res.json({ msg: "Hello" });
 });
 
 // Setting Express to listen on port 5000
