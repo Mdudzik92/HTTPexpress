c3a350617124ba579d42fcdb2355dd630785a0d2
Matt Dudzik <mdudzik92@gmail.com>
2020-12-24 10:13:51 -0600

    user-agent response

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-	res.send(req.header("host"));
+	res.send(req.header("user-agent"));
 });
 
 // Setting Express to listen on port 5000
