f78492cd83cce1fa1492d1de614415f23947d07c
Matt Dudzik <mdudzik92@gmail.com>
2020-12-24 10:40:23 -0600

    Sending a PUT request for id and title

--- server.js
+++ server.js
@@ -17,16 +17,13 @@ app.post("/contact", (req, res) => {
 	res.status(201).send(`Thank you ${req.body.name}`);
 });
 
-app.post("/login", (req, res) => {
-	if (!req.header("x-auth-token")) {
-		return res.status(400).send("No Token");
-	}
+app.put("/post/:id", (req, res) => {
+	// DATABASE STUFF
 
-	if (req.header("x-auth-token") !== "123456") {
-		return res.status(401).send("Not authorized");
-	}
-
-	res.send("Logged in");
+	res.json({
+		id: req.params.id,
+		title: req.body.title,
+	});
 });
 
 // Setting Express to listen on port 5000
