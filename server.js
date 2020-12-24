afb5d848d6785855098ca1e575c513083a89dcb2
Matt Dudzik <mdudzik92@gmail.com>
2020-12-24 10:20:18 -0600

    Sending name request only

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.post("/contact", (req, res) => {
-	res.send(req.body);
+	res.send(req.body.name);
 });
 
 // Setting Express to listen on port 5000
